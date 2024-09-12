bed98620137c00a6779777a39b6b2a44e145e7bd
Brian Penrose <brianpenrose@MacBook-Pro-2.local>
2024-09-12 11:31:52 +0100

    Updated package.json with homepage for GitHub Pages

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -37,7 +37,7 @@ const Navbar = ({ openModal, scrollToProjects }) => {
           <a href="/" className="nav-link" onClick={(e) => handleLinkClick(e, 'projects')}>Project</a>
         </li>
         <li className="nav-item">
-          <a href="/reactPortfolio/public/resume.pdf" target="_blank" className="nav-link">Resume</a>
+        <a href="/reactPortfolio/resume.pdf" target="_blank" className="nav-link">Resume</a>
         </li>
         <li className="nav-item">
           <a href="/" className="nav-link" onClick={(e) => handleLinkClick(e, 'contact')}>Contact</a>
